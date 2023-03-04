69a412e83df6e450176a4941e19074f8cfaa22e6
Graphikfm <fabienmari@outlook.fr>
2023-03-04 19:02:19 +0100

    my first update of released project

--- src/exo12.php
+++ src/exo12.php
@@ -5,7 +5,16 @@ require_once '../vendor/autoload.php';
 $loader = new \Twig\Loader\FilesystemLoader('../templates');
 $twig = new \Twig\Environment($loader);
 // ton code ici
+
+/* Connexion à une base MySQL avec l'invocation de pilote */
+$dsn = 'mysql:dbname=calendar;host=localhost:3306';
+$user = 'root';
+$password = '';
+
+$pdo = new \PDO('mysql:dbname=calendar;host=localhost:3306;charset=utf8', 'root', '');
+
 $today = new DateTime('today');
+var_dump($today->format('Y-m-d'));
 $currentYear = $today->format('Y');
 $year = $_GET['year'] ?? $currentYear;
 $minYear = $currentYear-5;
@@ -17,29 +26,22 @@ if (strlen($year) !== 4 || !is_numeric($year)){
 if ($year > $maxYear || $year < $minYear) {
     header('Location: /tailwindcss/src/exo12.php'); http://localhost/tailwindcss/src/exo12.php
 }
-$daysNames = [
-    1=>'Lun',
-    2=>'Mar',
-    3=>'Mer',
-    4=>'Jeu',
-    5=>'Ven',
-    6=>'Sam',
-    7=>'Dim'
-];
-$monthsNames = [
-    1=>'Janvier',
-    2=>'Fevrier',
-    3=>'Mars',
-    4=>'Avril',
-    5=>'Mais',
-    6=>'Juin',
-    7=>'Juillet',
-    8=>'Aout',
-    9=>'Septembre',
-    10=>'Octobre',
-    11=>'Novembre',
-    12=>'Decembre'
-];
+$requestWeekNameOfYear = $pdo->prepare('
+            SELECT MonthName
+            FROM calendar.dim_date
+            GROUP BY dim_date.MonthName
+            ORDER BY dim_date.Month
+    ');
+$requestWeekNameOfYear->execute();
+$monthsNames = [];
+$f = $requestWeekNameOfYear->fetchAll(PDO::FETCH_ASSOC);
+$arrayTest=[];
+foreach ($f as $key ) {
+    foreach ($key as $value) {
+        $monthsNames[] = $value;
+    }
+}
+var_dump($monthsNames);
 $arrayTest = [];
 $month = [];
 $monthprev = [];
@@ -69,97 +71,68 @@ foreach ($monthsNames as $monthNum=>$monthName) {
 //var_dump($fin);
 
 
-    /* Connexion à une base MySQL avec l'invocation de pilote */
-    $dsn = 'mysql:dbname=calendar;host=localhost:3306';
-    $user = 'root';
-    $password = '';
 
-    $pdo = new \PDO('mysql:dbname=calendar;host=localhost:3306', 'root', '');
 //var_dump($pdo);
 //print_r($pdo);
 //$stmt = $pdo->query('SELECT * FROM calendar ');
-
-
-
-    $stmt = $pdo->prepare('SELECT *,
-       IF (calendar.dim_date.weekDayFlag = "n",1,0)
-            AS is_weekend
-        
-            FROM calendar.dim_date
-            WHERE DateFull
-            BETWEEN :debut
-            AND :fin; '
-    );
-    $stmt->bindParam(':debut',$debut, PDO::PARAM_STR);
-    $stmt->bindParam(':fin',$fin, PDO::PARAM_STR);
-    $stmt->execute();
-
-// return $stmt->fetchAll(PDO::FETCH_ASSOC);
-// var_dump($stmt);
- $globalDataAarray = $stmt->fetchAll(PDO::FETCH_ASSOC);
-var_dump($globalDataAarray);
-
-// return $stmt->fetchAll(PDO::FETCH_ASSOC);
-// var_dump($stmt);
-
 
     // on vient appeler le tableau instancié en amont à vide et on y place en clé du tableau qui parcours les mois d'une année
     $month[$monthNum] = [
         'name'=>$monthName,
         'prevmonthdays' =>$prevmonthdays,
         'monthNum'=>$monthNum,
+        'days' => [],
+        'DaysName'=> [],
+
+
     ];
 
+    $requestWeekNameOfYear = $pdo->prepare('
+            SELECT MonthName
+            FROM calendar.dim_date
+            GROUP BY dim_date.MonthName
+            ORDER BY dim_date.Month
+    ');
+    $requestWeekNameOfYear->execute();
+    $month[$monthNum]['MonthName'] = $requestWeekNameOfYear->fetchAll(PDO::FETCH_ASSOC);
 
+    $requestDaysNameOfWeek = $pdo->prepare('
+            SELECT DayAbbrev
+            FROM calendar.dim_date
+            GROUP BY dim_date.DayAbbrev
+            ORDER BY dim_date.DayOfWeek
+    ');
+    $requestDaysNameOfWeek->execute();
+    $month[$monthNum]['DaysName'] = $requestDaysNameOfWeek->fetchAll(PDO::FETCH_ASSOC);
 
-// on crée une boucle qui parcours le tableau periode debut -> fin d'un mois et sa clé $day permet de parcourir tous les jours contenus dans un mois. et cela x12 grace a la premiere boucle des mois d'une année
-    foreach($globalDataAarray as $Days) {
-//var_dump($globalDataAarray);
-//        $Day['DayNumInMonth'];
-        $dataDaysOnYear = $Days['DayNumInMonth'];
-        $dataNameOnDays = $Days['DayAbbrev'];
-        $dataNumDayOfWeek = $Days['DayOfWeek'];
-        $NumOfMonth = $Days['Month'];
-        $yearY = $Days['Year'];
-        $fullDate = $Days['DateFull'];
-        $MonthName = $Days['MonthName'];
-        $IsWeekend = $Days['is_weekend'];
-//        var_dump($dataDaysOnYear);
-//        var_dump($globalDataAarray );
-        $month[$monthNum]['Days'][] = [
-            'DaysOnMonth'=> $dataDaysOnYear,
-            'DayAbbrev'=> $dataNameOnDays,
-            'DayOfWeek'=> $dataNumDayOfWeek,
-            'Month'=> $NumOfMonth,
-            'Year'=> $yearY,
-            'DateFull' => $fullDate,
-            'MonthName'=> $MonthName,
-            'is_weekend'=>$IsWeekend,
-        ];
-    }
-
+        $stmt = $pdo->prepare('SELECT *,
+       IF (calendar.dim_date.weekDayFlag = "n",1,0)
+            AS is_weekend
+            FROM calendar.dim_date
+            WHERE dim_date.DateFull
+            BETWEEN :debut
+            AND :fin; '
+        );
+        $stmt->bindParam(':debut',$debut, PDO::PARAM_STR);
+        $stmt->bindParam(':fin',$fin, PDO::PARAM_STR);
+        $stmt->execute();
+        $month[$monthNum]["days"] = $stmt->fetchAll(PDO::FETCH_ASSOC);
+var_dump($month[$monthNum]);
 }
-
-
 // fin de ton code
-
-
-
-
-
-//    var_dump($globalDataAarray);
 
 // and somewhere later:
 
 echo $twig->render('calendarDimDate.html.twig', [
-    'monthsNames' =>$month,
+    'months' =>$month,
     'monthsprev'=>$monthprev,
-    'daysNames'=>$daysNames,
     'today'=>$today,
     'year'=>$year,
     'currentYear'=>$currentYear,
     'allowedYears'=> $allowedYears,
     'maxYear'=> $maxYear,
-    'minYear'=> $minYear,
-    '$globalDataAarray'=>$globalDataAarray
+    'minYear'=> $minYear
+
 ]);
+
+
